97ca1b05027ce51cbe879a78ee10657269ee87d1
Miroslav Bambousek <mbambousek@gmail.com>
2015-02-19 21:43:11 +0100

    Commit after some time

--- extras/scripts/alaskab_utils.py
+++ extras/scripts/alaskab_utils.py
@@ -1,4 +1,5 @@
-import xbmc, sys
+import sys
+import xbmc, xbmcaddon, xbmcgui, xbmcplugin
 
 def movetile():
     label = xbmc.getInfoLabel("Skin.String(home."+str(i)+".label)")
@@ -30,39 +31,120 @@ def removetile():
     xbmc.executebuiltin("Skin.Reset("+str(sys.argv[2])+".bgwidget)")
     xbmc.executebuiltin("Skin.Reset("+str(sys.argv[2])+".bgwidget.type)")
 
-# -----------------------------------------------------------------------		
-		
-if sys.argv[1] in ['inserttile']:
-    if sys.argv[2]:
-        insertpoint = str(sys.argv[2])
-        insertpoint = insertpoint[-3:]
-    else:
-        exit
+def get_class_members(klass):
+    ret = dir(klass)
+    if hasattr(klass,'__bases__'):
+        for base in klass.__bases__:
+            ret = ret + get_class_members(base)
+    return ret
 
-    # Do 329 to 320
-    if int(insertpoint) < 320:
-        for i in reversed(range(320,330)):
-            o = i + 1
-            movetile()
-    else:
-        for i in reversed(range(int(insertpoint),330)):
-            o = i + 1
-            movetile()
-        removetile()
-        exit
+def uniq( seq ): 
+    """ the 'set()' way ( use dict when there's no set ) """
+    return list(set(seq))
+    
+def get_object_attrs( obj ):
+    # code borrowed from the rlcompleter module ( see the code for Completer::attr_matches() )
+    ret = dir( obj )
+    ## if "__builtins__" in ret:
+    ##    ret.remove("__builtins__")
 
-    # Need a special one for 312
-    if int(insertpoint) < 313:
-        i = 312
-        o = 320
-        movetile()
-    else:
-        removetile()
-        exit
+    if hasattr( obj, '__class__'):
+        ret.append('__class__')
+        ret.extend( get_class_members(obj.__class__) )
 
-    # Do 300 to 311
-    for i in reversed(range(int(insertpoint),312)):
-        o = i + 1
-        movetile()
-    removetile()
+        ret = uniq( ret )
+
+    return ret
+    
+def prepareShutdownMenu(window, list, buttonId):
+    if(xbmc.getCondVisibility("Control.IsVisible(%i)" % buttonId)):
+        button = window.getControl(buttonId)
+        listItem = xbmcgui.ListItem(label=button.getLabel())
+        listItem.setPath(path='plugin://plugin.bambi.utils?type=bzzz')
+        #xbmcplugin.addDirectoryItem( int( sys.argv[ 1 ] ), item2[ "file" ], liz, False, self.LIMIT )
+        list.addItem(listItem)
+        xbmc.log("adding %i" % buttonId)
+    
+class Utils:
+    def __init__(self):
+        self._parse_argv()
+        self.WINDOW_10111 = xbmcgui.Window(10111)
+
+        xbmc.log("self.TYPE: %s" % self.TYPE) 
+        xbmc.log("self.LIST_ID: %i" % self.LIST_ID) 
+        xbmc.log("self.START_BUTTON_ID: %i" % self.START_BUTTON_ID) 
+        xbmc.log("self.END_BUTTON_ID: %i" % self.END_BUTTON_ID) 
+        
+        
+    def _parse_argv( self ):
+        try:
+            params = dict( arg.split( "=" ) for arg in sys.argv[ 2 ].split( "&" ) )
+        except:
+            params = {}
+        self.TYPE = params.get( "?type", "loadShutdownMenu" )
+        self.LIST_ID = params.get( "listId", -1 )
+        self.START_BUTTON_ID = params.get( "startButtonId", -1 )
+        self.END_BUTTON_ID = params.get( "endButtonId", -1 )
+        
+# -----------------------------------------------------------------------   
+ 
+#xbmc.log('Starting Alaska utils') 
+#Utils()    
+#xbmc.log('Finished Alaska utils') 
+    
+DIALOGBUTTONMENU_PROPERTY_PREFIX = "AlaskaB.DialogButtonMenu.ListItem_%i."
+DIALOGBUTTONMENU_PROPERTY_LABEL = DIALOGBUTTONMENU_PROPERTY_PREFIX + "Label"
+DIALOGBUTTONMENU_PROPERTY_VISIBLE = DIALOGBUTTONMENU_PROPERTY_PREFIX + "Visible"
+DIALOGBUTTONMENU_PROPERTY_ONCLICK_1 = DIALOGBUTTONMENU_PROPERTY_PREFIX + "OnClick_1"
+DIALOGBUTTONMENU_PROPERTY_ONCLICK_2 = DIALOGBUTTONMENU_PROPERTY_PREFIX + "OnClick_2"
+    
+    
+if sys.argv[1] == 'prepareShutdownMenu':
+    window = xbmcgui.Window(10111)       
+    listItemList = list()
+    
+    for buttonId in range(int(sys.argv[3]), int(sys.argv[4]) + 1):
+        if(xbmc.getCondVisibility("Control.IsVisible(%i)" % buttonId)):
+            button = window.getControl(buttonId)
+            listItem = xbmcgui.ListItem(label=button.getLabel())
+            listItem.setProperty('onClick','Quit()')
+            listItem.setProperty('buttonId', str(buttonId))
+            listItemList.append(listItem)
+            xbmc.log("adding %i" % buttonId)
+
+    list = window.getControl(int(sys.argv[2]))
+    list.setStaticContent(listItemList)
+    
+    #list.reset()
+    
+    #for buttonId in range(int(sys.argv[3]), int(sys.argv[4]) + 1):
+    #    prepareShutdownMenu(window, list, buttonId)
+
+if sys.argv[1] == 'prepareShutdownMenu2':
+    window = xbmcgui.Window(10111)       
+    listItemList = list()
+    
+    for index in range(int(sys.argv[3]), int(sys.argv[4]) + 1):
+        xbmc.log(str(index))
+        xbmc.log(window.getProperty(DIALOGBUTTONMENU_PROPERTY_LABEL % index))
+        xbmc.log(window.getProperty(DIALOGBUTTONMENU_PROPERTY_VISIBLE % index))
+        xbmc.log(window.getProperty(DIALOGBUTTONMENU_PROPERTY_ONCLICK_1 % index))
+        xbmc.log(window.getProperty(DIALOGBUTTONMENU_PROPERTY_ONCLICK_2 % index))
+    
+        if(xbmc.getCondVisibility(window.getProperty(DIALOGBUTTONMENU_PROPERTY_VISIBLE % index))):
+            listItem = xbmcgui.ListItem(label=window.getProperty(DIALOGBUTTONMENU_PROPERTY_LABEL % index))
+            listItem.setProperty('onClick1',window.getProperty(DIALOGBUTTONMENU_PROPERTY_ONCLICK_1 % index))
+            onClick2 = window.getProperty(DIALOGBUTTONMENU_PROPERTY_ONCLICK_2 % index)
+            if onClick2:
+              listItem.setProperty('onClick2',onClick2)
+            listItemList.append(listItem)
+            xbmc.log("adding %i" % index)
+
+    list = window.getControl(int(sys.argv[2]))
+    list.setStaticContent(listItemList)
+
+elif sys.argv[1] == 'processShutdownMenu':    
+    xbmc.log('processShutdownMenu')
+
+    
 
